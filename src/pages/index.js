3b782b484abcfbe497e83e76e97dcf28928b930c
Maryna Ostapliuk <marynaum01@gmail.com>
2022-12-14 17:08:23 +0200

    loadable

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,21 @@ import Seo from 'components/Seo';
 import { Spinner } from 'components';
 import Hero from 'views/Hero/Hero';
 import Events from 'components/scripts/Events';
-// import About from 'views/About/About';
-// import Cultures from 'views/Cultures/Cultures';
-// import Personnel from 'views/Personnel/Personnel';
-// import TechnicalEquipments from 'views/TechnicalEquipments/TechnicalEquipments';
-// import Gallery from 'views/Gallery/Gallery';
-// import Contacts from 'views/Contacts/Contacts';
+import About from 'views/About/About';
+import Cultures from 'views/Cultures/Cultures';
+import Personnel from 'views/Personnel/Personnel';
+import TechnicalEquipments from 'views/TechnicalEquipments/TechnicalEquipments';
+import Gallery from 'views/Gallery/Gallery';
+import Contacts from 'views/Contacts/Contacts';
 
-const About = loadable(() => import('views/About/About'));
-const Cultures = loadable(() => import('views/Cultures/Cultures'));
-const Personnel = loadable(() => import('views/Personnel/Personnel'));
-const TechnicalEquipments = loadable(() =>
-  import('views/TechnicalEquipments/TechnicalEquipments'),
-);
-const Gallery = loadable(() => import('views/Gallery/Gallery'));
-const Contacts = loadable(() => import('views/Contacts/Contacts'));
+// const About = loadable(() => import('views/About/About'));
+// const Cultures = loadable(() => import('views/Cultures/Cultures'));
+// const Personnel = loadable(() => import('views/Personnel/Personnel'));
+// const TechnicalEquipments = loadable(() =>
+//   import('views/TechnicalEquipments/TechnicalEquipments'),
+// );
+// const Gallery = loadable(() => import('views/Gallery/Gallery'));
+// const Contacts = loadable(() => import('views/Contacts/Contacts'));
 
 const IndexPage = ({ data }) => {
   const [isLoading, setIsLoading] = useState(true);
